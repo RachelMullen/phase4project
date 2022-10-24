1f13c5d2f794d5a53b464cc985f52c779e15a510
Erica Hashert <ericahashert@gmail.com>
2022-10-24 13:33:32 -0600

    migrations + relationships

---
+++ app/serializers/like_serializer.rb
@@ -0,0 +1,3 @@
+class LikeSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :woof_id
+end
---
+++ db/migrate/20221024190002_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :name
+      t.string :breed
+      t.string :email
+      t.string :bio
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
