c8bcf75412b6a7e9d212fbecccffc7bd77a087c9
Erica Hashert <ericahashert@gmail.com>
2022-10-25 15:18:11 -0600

    just all the updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
-    has_secure_password
-
     has_many :comments
     has_many :woofs, through: :comments
+
+    has_secure_password
 
     # validates :username, presence: true
     # validates :username, uniqueness: true
     # validates :email, uniqueness: true
     # validates :email, presence: true
     # validates :name, presence: true
-    validates :password_digest, presence: true
+    # validates :password_digest, presence: true
     # validates :password_digest, uniqueness: true
+
 end
