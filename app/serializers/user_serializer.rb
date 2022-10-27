bd6e2843b04829a18ea2c30dee4b3438d18af757
Rachel Mullen <mullen.rachel@gmail.com>
2022-10-27 16:48:01 -0600

    serializers baby

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :name, :email, :bio, :image_url
   has_many :comments
+  has_many :woofs
 end
--- app/serializers/woof_serializer.rb
+++ app/serializers/woof_serializer.rb
@@ -1,4 +1,5 @@
 class WoofSerializer < ActiveModel::Serializer
   attributes :id, :image_url, :woof_content
   has_many :comments
+  has_many :users
 end
